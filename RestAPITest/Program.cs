92e17d192f204c6c32a2fa93acbe7a30d1b11e1e
rf1337 <rasmus.ferst@gmail.com>
2023-06-26 08:51:52 +0200

    Added appsettings to .gitignore

--- RestAPITest/Program.cs
+++ RestAPITest/Program.cs
@@ -15,6 +15,8 @@ namespace PortfolioRestAPI
             builder.Services.AddScoped<ISendService, SMSService>();
 
 
+            // Test, remove later
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
